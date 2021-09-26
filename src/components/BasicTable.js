df570f064b3ed885a0b8a5ecf25cf00be4c5bf8b
ukaran11 <ukaran11@gmail.com>
2021-09-26 16:49:04 +0530

    Utsav | Added styling

--- src/components/BasicTable.js
+++ src/components/BasicTable.js
@@ -2,6 +2,7 @@ import React, { useMemo } from 'react';
 import { useTable } from 'react-table';
 import MOCK_DATA from './MOCK_DATA.json';
 import { COLUMNS } from './columns';
+import './table.css';
 
 export const BasicTable = () => {
     const columns = useMemo(() => COLUMNS, [])
